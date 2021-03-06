b0af6e3bc9e4cdd0ecb6c604f5451a3d5bb2a0db
KAZI MUSTAKIM <KAZIMUSTAKIM39@GMAIL.COM>
2021-03-06 19:02:40 +0530

    some align fixed
    some align fixed

--- js/myscript.js
+++ js/myscript.js
@@ -158,3 +158,14 @@ $(function () {
     text.addClass("hidden").delay(2052).fadeOut("slow");
   }, 2000);
 });
+
+// project filter btn active
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
